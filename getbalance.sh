60dded5eefcf3d1594a213b9bfd0f858cc9459eb
buzzkillb <travanx@gmail.com>
2020-04-18 15:45:53 -0700

    use config.conf for settings

--- getbalance.sh
+++ getbalance.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-coin=Denarius
-rpcusername=RPCUSERNAME
-rpcpassword=RPCPASSWORD
-rpchost=127.0.0.1
-rpcport=32369
+. config.conf
 getbalance=$(curl --silent --data-binary '{"jsonrpc":"1.0","id":"curltext","method":"getbalance","params":[]}' -H 'content-type:text/plain;' http://$rpcusername:$rpcpassword@$rpchost:$rpcport/ | jq -r '.result')
 echo "$coin Balance: $getbalance"
