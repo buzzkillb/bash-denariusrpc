1521311eabf0aba1e19a61f3b04e81983ddb379a
buzzkillb <travanx@gmail.com>
2020-04-18 14:48:36 -0700

    remove ()

--- getbalance.sh
+++ getbalance.sh
@@ -4,5 +4,5 @@ rpcusername=RPCUSERNAME
 rpcpassword=RPCPASSWORD
 rpchost=127.0.0.1
 rpcport=32369
-getbalance=($(curl --silent --data-binary '{"jsonrpc":"1.0","id":"curltext","method":"getbalance","params":[]}' -H 'content-type:text/plain;' http://$rpcusername:$rpcpassword@$rpchost:$rpcport/ | jq -r '.result'))
+getbalance=$(curl --silent --data-binary '{"jsonrpc":"1.0","id":"curltext","method":"getbalance","params":[]}' -H 'content-type:text/plain;' http://$rpcusername:$rpcpassword@$rpchost:$rpcport/ | jq -r '.result')
 echo "$coin Balance: $getbalance"
