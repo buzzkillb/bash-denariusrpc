28789f2f3492e3c8d285a7da41795b0bc6fec0dd
buzzkillb <travanx@gmail.com>
2020-04-19 13:51:55 -0700

    add {} to variable

--- getblockcount.sh
+++ getblockcount.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . config.conf
 getblockcount=$(curl --silent --data-binary '{"jsonrpc":"1.0","id":"curltext","method":"getblockcount","params":[]}' -H 'content-type:text/plain;' http://"${rpcusername}":"${rpcpassword}"@"${rpchost}":"${rpcport}"/ | jq -r '.result')
-printf '%s\n' "$getblockcount"
+printf '%s\n' "${getblockcount}"
