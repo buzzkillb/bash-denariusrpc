ddaf727332bd48e3631819e72c353bdba6bd81da
buzzkillb <travanx@gmail.com>
2020-04-18 17:14:20 -0700

    Update unspentaddresses.sh

--- examples/unspentaddresses.sh
+++ examples/unspentaddresses.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-unspentaddresses=$(./listunspent.sh | jq -r '.address' | awk '!seen[$0]++')
+unspentaddresses=$(./listunspent.sh | jq -r '.[].address' | awk '!seen[$0]++')
 echo "$unspentaddresses"
