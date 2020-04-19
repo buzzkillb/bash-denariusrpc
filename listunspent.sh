a16778104b24d3af8114b0f68ac6c036880637ca
buzzkillb <travanx@gmail.com>
2020-04-18 17:10:58 -0700

    remove [] from result

--- listunspent.sh
+++ listunspent.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . config.conf
-listunspent=$(curl --silent --data-binary '{"jsonrpc":"1.0","id":"curltext","method":"listunspent","params":[]}' -H 'content-type:text/plain;' http://$rpcusername:$rpcpassword@$rpchost:$rpcport/ | jq -r '.result[]')
+listunspent=$(curl --silent --data-binary '{"jsonrpc":"1.0","id":"curltext","method":"listunspent","params":[]}' -H 'content-type:text/plain;' http://$rpcusername:$rpcpassword@$rpchost:$rpcport/ | jq -r '.result')
 echo "$listunspent"
