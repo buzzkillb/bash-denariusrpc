7feca944412c36ed42f01580e633348d09edc435
buzzkillb <travanx@gmail.com>
2020-04-20 12:39:41 -0700

    Create masternode_monitor.sh

---
+++ examples/masternode_monitor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+. config.conf
+. rpc.sh
+rpc_masternode_status
+_masternodestatus=$(eval ${masternodeStatus} | jq -r '.[] | select(.status=="notfound") | .alias,.status ') 
+printf '%s\n' "${_masternodestatus}" | (
+    while read alias; do
+        read status
+        echo 'alias:' $alias
+        echo 'status:' $status
+        rpc_masternode_startalias "$alias"
+	_masternodestartalias=$(eval "${masternodeStartAlias}")
+	printf '%s\n' "${_masternodestartalias}"
+    done
+)
