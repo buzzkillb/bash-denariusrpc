ffd5d713713f8670d8cd74d0c8f0398602d69348
buzzkillb <travanx@gmail.com>
2020-04-20 11:38:16 -0700

    add masternode status

--- rpc.sh
+++ rpc.sh
@@ -25,3 +25,7 @@ rpc_sendrawtransaction () {
 sendRawTransaction="curl -s -d '{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"sendrawtransaction\",\"params\":["${1}"]}' -H 'content-type:text/plain;' http://"${rpcusername}":"${rpcpassword}"@"${rpchost}":"${rpcport}"/"
 local sendRawTransaction
 }
+rpc_masternode_status () {
+masternodeStatus="curl -s -d '{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"masternode\",\"params\":[\"status\"]}' -H 'content-type:text/plain;' http://"${rpcusername}":"${rpcpassword}"@"${rpchost}":"${rpcport}"/ | jq -r '.result'"
+local masternodeStatus
+}
