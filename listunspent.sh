afa8011b82451d6d5e290e37f19aed6b2f24d327
buzzkillb <travanx@gmail.com>
2020-04-19 23:38:49 -0700

    use rpc.sh function

--- listunspent.sh
+++ listunspent.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 . config.conf
-listunspent=$(curl --silent --data-binary '{"jsonrpc":"1.0","id":"curltext","method":"listunspent","params":[]}' -H 'content-type:text/plain;' http://$rpcusername:$rpcpassword@$rpchost:$rpcport/ | jq -r '.result')
-echo "$listunspent"
+. rpc.sh
+listunspent=$(rpc_listunspent | jq -r '.[]')
+printf '%s\n' "${listunspent}"
