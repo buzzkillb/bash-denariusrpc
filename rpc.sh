cb8b3b1c483e99f21e442ffdd386a4f5fa635a11
buzzkillb <travanx@gmail.com>
2020-04-22 23:33:27 -0700

    add jupiter_pod function

--- rpc.sh
+++ rpc.sh
@@ -49,3 +49,7 @@ rpc_jupiter_upload () {
 jupiterUpload="curl -s -d '{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"jupiterupload\",\"params\":[\""${1}"\"]}}' -H 'content-type:text/plain;' http://"${rpcusername}":"${rpcpassword}"@"${rpchost}":"${rpcport}"/ | jq -r '.result'"
 local jupiterUpload
 }
+rpc_jupiter_pod () {
+jupiterPod="curl -s -d '{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"jupiterpod\",\"params\":[\""${1}"\"]}}' -H 'content-type:text/plain;' http://"${rpcusername}":"${rpcpassword}"@"${rpchost}":"${rpcport}"/ | jq -r '.result'"
+local jupiterPod
+}
