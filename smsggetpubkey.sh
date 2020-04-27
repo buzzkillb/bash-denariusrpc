30a564525ce383e61b8be3635fbd1e393d2200d3
buzzkillb <travanx@gmail.com>
2020-04-26 19:10:07 -0700

    Create smsggetpubkey.sh

---
+++ smsggetpubkey.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+. config.conf
+. rpc.sh
+
+#uses functions: rpc_smsggetpubkey
+
+#options: input address in your own wallet that's sent/received coins
+smsgGetPubKey=D6jievtTm6SxFPpywDa7yaAJAYMBrqh8Te
+
+rpc_smsggetpubkey "$smsgGetPubKey"
+_smsggetpubkey=$(eval "${smsgGetpubkey}")
+
+printf '%s\n' "${_smsggetpubkey}" | jq -r '.result'
+printf '%s\n' "${_smsggetpubkey}" | jq -r '."address in wallet"'
+printf '%s\n' "${_smsggetpubkey}" | jq -r '."compressed public key"'
+
+#store public key in compressedpublickey
+compressedpublickey=$(eval "${smsgGetpubkey}" | jq -r '."compressed public key"')
+echo $compressedpublickey
