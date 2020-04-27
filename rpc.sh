874e592cc2857a71fd65f54b3620698a09a81319
buzzkillb <travanx@gmail.com>
2020-04-26 19:08:53 -0700

    add smsggetpubkey function

--- rpc.sh
+++ rpc.sh
@@ -56,6 +56,10 @@ rpc_jupiter_pod () {
 jupiterPod="curl -s -d '{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"jupiterpod\",\"params\":[\""${1}"\"]}}' -H 'content-type:text/plain;' http://"${rpcusername}":"${rpcpassword}"@"${rpchost}":"${rpcport}"/ | jq -r '.result'"
 local jupiterPod
 }
+rpc_smsggetpubkey () {
+smsgGetpubkey="curl -s -d '{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"smsggetpubkey\",\"params\":[\""${1}"\"]}' -H 'content-type:text/plain;' http://"${rpcusername}":"${rpcpassword}"@"${rpchost}":"${rpcport}" | jq -r '.result'"
+local smsgGetpubkey
+}
 rpc_smsgsend () {
 smsgSend="curl -s -d '{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"smsgsend\",\"params\":[\""${1}"\", \""${2}"\", \""${3}"\"]}' -H 'content-type:text/plain;' http://"${rpcusername}":"${rpcpassword}"@"${rpchost}":"${rpcport}" | jq -r '.result'"
 local smsgSend
